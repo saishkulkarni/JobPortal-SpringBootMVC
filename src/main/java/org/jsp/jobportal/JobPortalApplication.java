da6c5cac840357cabcb3bd603acbd80caec1b0ce
saishkulkarni <saishkulkarni7@gmail.com>
2023-10-27 15:57:44 +0530

    project created

---
+++ src/main/java/org/jsp/jobportal/JobPortalApplication.java
@@ -0,0 +1,13 @@
+package org.jsp.jobportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jsp/jobportal/JobPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jsp.jobportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
